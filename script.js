9e86fe34edeb471b97b04299bb8ec92f14b323c5
Anastasia <sarl.amcc.j@gmail.com>
2022-02-07 15:15:00 +0100

    ajout script.js pour donation.html, change color

---
+++ script.js
@@ -0,0 +1,15 @@
+let classiqueFormule = document.getElementById("classiqueFormule")
+let premiumFormule = document.getElementById("premiumFormule")
+let prestigeFormule = document.getElementById("prestigeFormule")
+
+classiqueFormule.addEventListener("click", ()=>{
+    alert("Merci pour votre don, mais vous pouvez faire mieux !")
+})
+
+premiumFormule.addEventListener("click", ()=>{
+    alert("Merci pour votre don, encore un petit effort !")
+})
+
+prestigeFormule.addEventListener("click", ()=> {
+    alert("Merci pour votre don, vous avez un grand coeur d'éléphant!")
+})
